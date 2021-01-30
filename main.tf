432c34f27de447b9cb37d88efc433c7696c84e60
kuniaki <k.ishikawa.0128@gmail.com>
2021-01-30 10:08:31 +0900

    sample for data store

---
+++ main.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+locals {
+  example_instance_type = "t3.micro"
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = local.example_instance_type
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
+}
