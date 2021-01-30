12d459462ac017aafb5e48fc10c53894ba76ad32
kuniaki <k.ishikawa.0128@gmail.com>
2021-01-30 15:04:28 +0900

    sample for function

--- main.tf
+++ main.tf
@@ -30,12 +30,7 @@ resource "aws_instance" "example" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.example_ec2.id]
-
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+  user_data = file("./user_data.sh")
 }
 
 output "example_public_dns" {
