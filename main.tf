2ededcc3b6040e31cff07c0e89502ce64fb96049
kuniaki <k.ishikawa.0128@gmail.com>
2021-01-30 14:51:08 +0900

    sample for security group & apache

--- main.tf
+++ main.tf
@@ -1,27 +1,43 @@
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
+  ingress = [{
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "example_ec2_ingress"
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self = false
+  }]
 
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-}
-
-locals {
-  example_instance_type = "t3.micro"
+  egress = [{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "example_ec2_egress"
+    ipv6_cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+    security_groups = []
+    self = false
+  }]
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = local.example_instance_type
+  ami           = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
 
-output "example_instance_id" {
-  value = aws_instance.example.id
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
 }
